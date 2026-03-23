6f1e84d8fe237b1961239359397fa5b2193164d5
alas <janreytanjutco15@gmail.com>
2026-03-23 13:40:26 +0800

    Update pizzaController.js

--- controllers/pizzaController.js
+++ controllers/pizzaController.js
@@ -1,64 +1,57 @@
-//SQL
-const connection = require('../config/db');
+const connection = require('../config/db'); // your MySQL connection
 
-//Get all users
-exports.getAllUsers=(req,res)=>{
-    connection.query('SELECT * FROM userdata', (err,rows,fields)=>{
-        if(err) throw err;
-            res.json(rows);
+// Get all users
+exports.getAllUsers = (req, res) => {
+    connection.query('SELECT * FROM userdata', (err, rows) => {
+        if (err) throw err;
+        res.json(rows);
     });
 };
 
-//Search a user by ID
-exports.getUsersById=(req,res)=>{
-    const id=req.params.id;
-    connection.query('SELECT * FROM userdata WHERE id=?',[id],(err,rows,fields)=>{
-        if(err) throw err;
-        if(rows.length>0)
-            res.json(rows);
-        else
-            res.status(404).json({message: 'User not found'});
+// Get user by ID
+exports.getUserById = (req, res) => {
+    const id = req.params.id;
+    connection.query('SELECT * FROM userdata WHERE id = ?', [id], (err, rows) => {
+        if (err) throw err;
+        if (rows.length > 0) res.json(rows[0]);
+        else res.status(404).json({ message: 'User not found' });
     });
 };
 
-//Create a new user
-//CRUD - Create
-exports.createUser=(req,res)=>{
-    const {fname,lname,email,gender,ip_add} = req.body;
-    connection.query('INSERT INTO userdata (first_name, last_name, email, gender, ip_address) VALUES (?,?,?,?,?)',
-      [fname,lname,email,gender,ip_add],(err,result)=>{
-        if(err) throw err;
-            res.json({message: 'User Created Succesfully', userId:result.insertId});
-    });
+// Create new user
+exports.createUser = (req, res) => {
+    const { fname, lname, email, gender, ip_add } = req.body;
+    connection.query(
+        'INSERT INTO userdata (first_name, last_name, email, gender, ip_address) VALUES (?, ?, ?, ?, ?)',
+        [fname, lname, email, gender, ip_add],
+        (err, result) => {
+            if (err) throw err;
+            res.status(201).json({ message: 'User created', userId: result.insertId });
+        }
+    );
 };
 
-// Update a user
-// CRUD - Update
+// Update user
 exports.updateUser = (req, res) => {
-  const {fname,lname,email,gender,ip_add,id } = req.body;
-  connection.query("UPDATE userdata SET first_name=?, last_name=?, email=?, gender=?, ip_address=? WHERE id=?",
-    [fname,lname,email,gender,ip_add,id],
-    (err, result) => {
-      if (err) throw err;
-      if (result.affectedRows > 0) {
-        res.json({ message: "User Updated Successfully" });
-      } else {
-        res.status(404).json({ message: "User Not Found" });
-      }
-    }
-  );
+    const id = req.params.id;
+    const { fname, lname, email, gender, ip_add } = req.body;
+    connection.query(
+        'UPDATE userdata SET first_name=?, last_name=?, email=?, gender=?, ip_address=? WHERE id=?',
+        [fname, lname, email, gender, ip_add, id],
+        (err, result) => {
+            if (err) throw err;
+            if (result.affectedRows > 0) res.json({ message: 'User updated' });
+            else res.status(404).json({ message: 'User not found' });
+        }
+    );
 };
 
-//Delete a user
-//CRUD - Delete
-exports.deleteUser = (req,res) => {
-  const id = req.body.id;
-  connection.query("DELETE FROM userdata WHERE id=?",[id],(err, result) => {
-    if (err) throw err;
-    if (result.affectedRows > 0) {
-      res.json({ message: "User Deleted Successfully" });
-    } else {
-      res.status(404).json({ message: "User Not Found" });
-    }
-  });
+// Delete user
+exports.deleteUser = (req, res) => {
+    const id = req.params.id;
+    connection.query('DELETE FROM userdata WHERE id=?', [id], (err, result) => {
+        if (err) throw err;
+        if (result.affectedRows > 0) res.json({ message: 'User deleted' });
+        else res.status(404).json({ message: 'User not found' });
+    });
 };
